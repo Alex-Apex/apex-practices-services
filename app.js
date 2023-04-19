c444c6948f5ca72872f4386118c36c24c572ffd3
Alex-Apex <agomez@apexsystems.com>
2023-04-18 19:36:24 -0600

    Added a new Employees route

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
+const employeeRouter = require('./routes/employees');
 const app = express();
-app.post('/employees', (req, res) => { /*...*/ });
-app.get('/employees', (req, res) => { /*...*/ });
-app.get('/employees/:id', (req, res) => { /*...*/ });
-app.put('/employees/:id', (req, res) => { /*...*/ });
-app.delete('/employees/:id', (req, res) => { /*...*/ });
-
+app.use('/employees',employeeRouter);
 const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
---
+++ routes/employees.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/', (req, res) => { /*...*/ });
+router.get('/', (req, res) => { /*...*/ });
+router.get('/:id', (req, res) => { /*...*/ });
+router.put('/:id', (req, res) => { /*...*/ });
+router.delete('/:id', (req, res) => { /*...*/ });
+
+module.exports = router;
