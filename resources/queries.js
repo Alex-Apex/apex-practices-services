d9e65d7eef7ca2f1b87623e2e2a4af00070cb606
Alex-Apex <agomez@apexsystems.com>
2023-04-25 19:48:56 -0600

    added ESLint support with Google style

--- resources/queries.js
+++ resources/queries.js
@@ -1,5 +1,12 @@
 const queries = {
     GET_ALL_EMPLOYEES: `SELECT * FROM Employees`,
+    GET_BENCH_REPORT: `SELECT TOP (1000) [Practice]
+                            ,[EmpID]
+                            ,[UserName]
+                            ,[Name]
+                            ,[Status]
+                            ,[OnBenchSince]
+                        FROM [dbo].[BENCHMANAGEMENT]`,
     // Add more queries here 
 };
 
